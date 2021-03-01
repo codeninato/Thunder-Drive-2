cf61173c9e80673083f045695e232b76ce6b6752
codeninato <69345945+codeninato@users.noreply.github.com>
2021-03-01 17:45:45 +0530

    Add files via upload

---
+++ Procfile
@@ -0,0 +1,1 @@
+worker: python drive.py
---
+++ drive.py
@@ -0,0 +1,116 @@
+from pyrogram import Client, filters
+from pyrogram.errors import RPCError
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from pydrive.auth import AuthenticationError, GoogleAuth
+from pydrive.drive import GoogleDrive
+from pydrive.files import ApiRequestError
+import os
+
+try:
+    bot = Client(
+        session_name="thunder_drive",
+        api_id="1754216",
+        api_hash="fd87630056c7f56e64c615d56641bfec",
+        bot_token="1541590664:AAFIahIn_XnfVjIdiUvy2YeCUWSBb11uMFk"
+    )
+
+    gAuth = None
+
+    @bot.on_message(filters.private & filters.incoming & filters.command(['auth']))
+    def handle_auth(client, message):
+        global gAuth
+        
+        if gAuth is None:
+            gAuth = GoogleAuth()
+
+        gAuth.LoadCredentialsFile("credentials.json")
+
+        if gAuth.credentials is None:
+            URL = gAuth.GetAuthUrl()
+            message.reply_text(
+                "1. Visit this URL\n2. Copy the Authorization Code after giving necessary permissions.\n3. Send the Authorization Code here",
+                reply_markup = InlineKeyboardMarkup(
+                    [[InlineKeyboardButton("Authorization URL", url=URL)]]
+                )
+            )
+        elif gAuth.access_token_expired:
+            gAuth.Refresh()
+            message.reply_text(
+                "Google Drive Token Refreshed Successfully."
+            )
+            gAuth.SaveCredentialsFile("credentials.json")
+            gAuth = None
+        else:
+            gAuth.Authorize()
+            message.reply_text(
+                "Google Drive Authorized Successfully."
+            )
+            gAuth.SaveCredentialsFile("credentials.json")
+            gAuth = None
+    
+    @bot.on_message(filters.private & filters.incoming & filters.command(['token']))
+    def handle_token(client, message):
+        global gAuth
+        token = message.command[1].strip()
+
+        if len(token) == 62 and token[1] == "/":
+            if gAuth is not None:
+                try:
+                    gAuth.Auth(token)
+                    gAuth.SaveCredentialsFile("credentials.json")
+                    message.reply_text(
+                        "Authentication Successful !"
+                    )
+                    gAuth = None
+                except AuthenticationError:
+                    message.reply_text(
+                        "Authentication Failed !"
+                    )
+            else:
+                message.reply_text(
+                    "No Authentication in progress ! Start an Authentication process first."
+                )
+        else:
+            message.reply_text(
+                "Invalid Token Provided.",
+                quote = True
+            )
+
+    @bot.on_message(filters.private & filters.incoming & ~filters.text & filters.media) 
+    def handle_media(client, message):
+        gauth = GoogleAuth()
+
+        gauth.LoadCredentialsFile("credentials.json")
+
+        if gauth.credentials is None or gauth.access_token_expired:
+            message.reply_text(
+                "No Authentication Found! Authenticate Google Drive using 'auth' command.",
+                quote = True
+            )
+        else:
+            drive = GoogleDrive(gauth)
+            status_msg = message.reply_text("Downloading file to server...", quote = True)
+            file = bot.download_media(message)
+            status_msg.edit("Uploading file to Google Drive...")
+            driveFile = drive.CreateFile({
+                'title': message.caption,
+                'parents': [{
+                    'kind': 'drive#fileLink',
+                    'teamDriveId': '0AH0pUxFCGp6vUk9PVA',
+                    'id': '18T-dBF4pdkGUQZx3D25Q7ihiPpsI8HUt'
+                }]
+            })
+            driveFile.SetContentFile(file)
+            try:
+                driveFile.Upload(param={'supportsTeamDrives': True})
+                message.reply_text(
+                    "File successfully uploaded to Google Drive!",
+                    quote = True
+                )
+                status_msg.delete(revoke = True)
+            except ApiRequestError:
+                status_msg.edit("File Upload Failed!")
+
+    bot.run()
+except RPCError:
+    pass
